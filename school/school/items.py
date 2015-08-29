cc7fbd70bb8a415992d13a9df46743812e18cca3
Usman Khaliq <usmankhaliq@Usmans-MacBook-Pro.local>
2015-08-29 13:10:22 +0500

    code is reading all data from each state page

---
+++ school/school/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class SchoolItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field() 
+    address = scrapy.Field()
+    rating = scrapy.Field()
+    enrollment = scrapy.Field() 
+    grade_span = scrapy.Field()
+    
