8f3dffe938f675508bc6e061f33189254507a70d
Usman Khaliq <usmankhaliq@Usmans-MacBook-Pro.local>
2015-08-29 13:30:28 +0500

    reading data from all of the state school's first pages

--- school/school/spiders/states_spider.py
+++ school/school/spiders/states_spider.py
@@ -5,34 +5,24 @@ class StateSpider(scrapy.Spider):
     name = "states"
     allowed_domains = ["city-data.com"]
     start_urls = [
-        "http://www.city-data.com/schools-dirs/schools-PA.html"
+        "http://www.city-data.com/indexes/schools/"
        
     ]
 
-    #def parse(self, response):
-     #   for href in response.css("ul.tab-list.tab-list-short > li > a::attr('href')"): 
-      #  	url = response.urljoin(href.extract()) 
-#		yield scrapy.Request(url,callback=self.parse_tab_contents) 
+    def parse(self, response):
+        for href in response.css("ul.tab-list.tab-list-short > li > a::attr('href')"): 
+        	url = response.urljoin(href.extract()) 
+		yield scrapy.Request(url,callback=self.parse_tab_contents) 
 
 
- #   def parse_tab_contents(self,response):
-#	for sel in response.xpath('//div[@class="table-responsive"]/tbody/tr'): 
-#		item = SchoolItem() 
-#		item['name'] = sel.xpath('td[1]/text()').extract() 
-#		item['address']=sel.xpath('td[2]/text()').extract()
-#		item['rating']=sel.xpath('td[3]/text()').extract()
-#		item['enrollment']=sel.xpath('td[4]/text()').extract() 
-#		item['grade_span']=sel.xpath('td[5]/text()').extract() 
-#		yield item				
+    def parse_tab_contents(self,response):
+	for sel in response.xpath('//table[@class="table"]/tbody/tr'): 
+		item = SchoolItem() 
+		item['name'] = sel.xpath('td[1]/a/text()').extract() 
+		item['address']=sel.xpath('td[2]/text()').extract()
+		item['rating']=sel.xpath('td[3]/text()').extract()
+		item['enrollment']=sel.xpath('td[4]/text()').extract() 
+		item['grade_span']=sel.xpath('td[5]/text()').extract() 
+		yield item				
         
-    def parse(self,response): 
-        for sel in response.xpath('//table[@class="table"]/tbody/tr'):
-	               item = SchoolItem()
-	               item['name'] = sel.xpath('td[1]/a/text()').extract()
-	               item['address']=sel.xpath('td[2]/text()').extract()
-	               item['rating']=sel.xpath('td[3]/text()').extract()
-	               item['enrollment']=sel.xpath('td[4]/text()').extract()
-	               item['grade_span']=sel.xpath('td[5]/text()').extract() 
-		       yield item
-	               #print name,address,rating,enrollment,grade_span
 
